c7815804380f6f8a0d0dd7cf4e7fd2355c2696ae
vovacelidze <louglescom@gmail.com>
2023-06-12 17:22:30 +0300

    finished testimonials section for all devices

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import './testimonials.css'
+import './testimonials.css';
+import {data} from './data'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Testimonials = () => {
   return (
-    <section id='testimonials' > Testimonials </section>
+    <section id='testimonials' >
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data &&
+          data.map(({id, avatar, name, review}) => (
+          <SwiperSlide key={id} className="testimonial">
+            <div className="client__avatar">
+              <img  src={avatar} alt='Ava one' />
+            </div>
+            <h5 className='client__name'> {name} </h5>
+            <small className='client__review'>
+              {review}
+            </small>
+          </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </section>
   );
 };
 
---
+++ src/components/testimonials/data.js
@@ -0,0 +1,31 @@
+import Avatar1 from '../../assets/avatar1.jpg';
+import Avatar2 from '../../assets/avatar2.jpg';
+import Avatar3 from '../../assets/avatar3.jpg';
+import Avatar4 from '../../assets/avatar4.jpg';
+
+export const data = [
+  {
+    id: 1,
+    avatar: Avatar1,
+    name: 'Tina Show',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores.'
+  },
+  {
+    id: 2,
+    avatar: Avatar2,
+    name: 'Shatta Wale',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores.'
+  },
+  {
+    id: 3,
+    avatar: Avatar3,
+    name: 'Kwame Despite',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores.'
+  },
+  {
+    id: 4,
+    avatar: Avatar4,
+    name: 'Pepe Guardiola',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores.'
+  },
+]
